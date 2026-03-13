0276ca976f05abf8afc5cc5bcf7606244c1a984f
Cyxzhar <u2058201@st.ygu.ac.jp>
2026-03-13 16:20:18 +0900

    chore: add vite proxy for local API backend server

--- vite.config.js
+++ vite.config.js
@@ -46,6 +46,15 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   build: {
     rollupOptions: {
       output: {
